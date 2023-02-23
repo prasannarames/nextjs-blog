e39356f88c6685a7d9b38e4ad26e0467e53aa6a5
prasannarames <prasannaramesh98@gmail.com>
2023-02-23 13:37:59 +0530

    ups

--- pages/posts/FirstPost.js
+++ pages/posts/FirstPost.js
@@ -1,5 +1,11 @@
-export default function FirstPost(){
-    return (
-        <h1> first post</h1>
-    )
+import Link from "next/link";
+export default function FirstPost() {
+  return (
+    <>
+      <h1> first post</h1>
+      <h2>
+        <Link href="/">Bact to Home</Link>
+      </h2>
+    </>
+  );
 }
