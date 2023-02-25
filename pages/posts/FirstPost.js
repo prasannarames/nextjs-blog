763f98076b4ad696e502b64e13ff2cef7d894427
prasannarames <prasannaramesh98@gmail.com>
2023-02-25 22:27:55 +0530

    ups

--- pages/posts/FirstPost.js
+++ pages/posts/FirstPost.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1> first post</h1>
       <h2>
         <Link href="/">Bact to Home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
